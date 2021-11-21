275381dbb7a26a66bb93f3b9efbb721ac58ed208
Mikko <mikko.karhavirta@student.laurea.fi>
2021-11-21 19:02:36 +0200

    Splash and Login screens

---
+++ firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from "firebase";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+//import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCO1mL5hsiKqe9OWuTvo2Gb_HzyfVeRJzo",
+  authDomain: "fir-auth-86358.firebaseapp.com",
+  projectId: "fir-auth-86358",
+  storageBucket: "fir-auth-86358.appspot.com",
+  messagingSenderId: "63810727117",
+  appId: "1:63810727117:web:318f4627174f6ed5828d84"
+};
+
+// Initialize Firebase
+let app;
+if (firebase.apps.length === 0) {
+    app = firebase.initializeApp(firebaseConfig)
+} else {
+    app = firebase.app()
+}
+
+const auth = firebase.auth();
+
+export { auth };
